ac8f254d04490c5ea7b9aa6bc748372e317d9a9c
Rockefeller <pvapa15@mail.ru>
2022-07-07 21:16:14 +0300

    - Add changes in project
    Add:
    - Foldres: Scripts, Sprites.
    - Scripts: CrossManager and ZeroManager, and also added event handler.
    - On and off cross/zero on sce scene.

---
+++ Assets/Scripts/ZeroManager/ZeroManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZeroManager : MonoBehaviour
+{
+    public void OnOffButton(GameObject zero)
+    {
+        zero.SetActive(true);
+    }
+}
